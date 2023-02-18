5d214d9ec80b8796282f018fd21caa3b7805a5bc
ReyAlexandr <i.am.the.alexandr@gmail.com>
2023-02-18 11:12:51 +0530

    some changes

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ var swiper = new Swiper(".mySwiper1", {
 
 var swiper2 = new Swiper(".mySwiper2", {
   loop : true,
-  slidesPerView : 4,
-  loopedSlides : 4,
+  slidesPerView : 3,
+  loopedSlides : 3,
   // loopPreventsSliding : false,
   pagination: {
     el: ".swiper-pagination",
@@ -26,20 +26,7 @@ var swiper2 = new Swiper(".mySwiper2", {
 
 const nav = document.querySelector('nav');
 
-var img1_sec1 = document.querySelector('footer');
-
-var ob1 = new IntersectionObserver( es=> {
-  nav.style.background = es[0].isIntersecting ? 'blue' : 'var(--c1)';
-});
-
 window.addEventListener('scroll', () => {
-  if(window.scrollY > 100) {
-    ob1.observe(img1_sec1);
-    nav.style.boxShadow = '0px 5px 5px rgba(0,0,0,0.2)';
-  }
-  else {
-    ob1.disconnect();
-    nav.style.background = 'transparent';
-    nav.style.boxShadow = 'none';
-  }
+
+  nav.classList.toggle('nav-scroll', window.scrollY > 100);
 })
