dde67b09a86bcabfb1b9f3b71c6caff8a034afe4
ReyAlexandr <i.am.the.alexandr@gmail.com>
2023-02-23 00:16:05 +0530

    testing football web

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-/* =============================================================================== */
 
-var swiper = new Swiper(".mySwiper1", {
-  loop : true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
+var menuBar = document.querySelector('.fa-bars');
+var menuX = document.querySelector('.fa-xmark');
+var menu = document.querySelector('.menu');
+
+menuBar.addEventListener('click', ()=>{
+    menuBar.style.display = "none";
+    menuX.style.display = "inline-block";
+    menu.classList.toggle('menu-open'); 
 });
 
-/* =============================================================================== */
-
-
-var swiper2 = new Swiper(".mySwiper2", {
-  loop : true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-  breakpoints : {
-    600: {
-      slidesPerView:1,
-      loopedSlides:1
-    },
-    1100:{
-      slidesPerView : 3,
-      loopedSlides : 3,    
-    }
-  }
+menuX.addEventListener('click', ()=>{
+    menuBar.style.display = "inline-block";
+    menuX.style.display = "none";
+    menu.classList.toggle('menu-open'); 
 });
-
-/* =============================================================================== */
-
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-
-  nav.classList.toggle('nav-scroll', window.scrollY > 100);
-})
