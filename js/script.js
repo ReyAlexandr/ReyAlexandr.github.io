8df52af974c18509e13321ffd3d3e8a9e475713c
ReyAlexandr <i.am.the.alexandr@gmail.com>
2023-03-04 23:50:18 +0530

    league titles swiping set

--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@
 var menuBar = document.querySelector('.fa-bars');
 var menuX = document.querySelector('.fa-xmark');
 var menu = document.querySelector('.menu');
-var leagueTitles = document.querySelectorAll('.titles span');
+var titlesSpan = document.querySelectorAll('.titles span');
 var selectedTitle = document.querySelector('.selected');
 var matches = document.querySelectorAll('.matches');
 var news = document.querySelectorAll('.news');
-
 
 menuBar.addEventListener('click', ()=>{
     menuBar.style.display = "none";
@@ -20,13 +19,39 @@ menuX.addEventListener('click', ()=>{
     menu.classList.toggle('menu-open'); 
 });
 
-leagueTitles.forEach(title => {
+
+var leagueTitles = document.querySelector('.league-titles');
+var titlesContainer = document.querySelector('.titles');
+var startX, left;
+leagueTitles.addEventListener('touchstart', (event) => {
+    startX = event.touches[0].clientX;
+    left = Number(titlesContainer.style.left.slice(0,-2));
+});
+
+leagueTitles.addEventListener('touchmove', (event) => {
+    touch = event.touches[0].clientX;
+    change = touch - startX + left;
+    maxChange = leagueTitles.clientWidth - titlesContainer.clientWidth;
+    if (change >= maxChange) {
+        if(change <= 2) {
+            titlesContainer.style.left = `${change}px`;
+        }
+    }  
+    event.preventDefault();
+})
+
+leagueTitles.addEventListener('touchend', (event) => {
+    startX = event.changedTouches[0].clientX;
+});
+
+
+titlesSpan.forEach(title => {
     title.addEventListener('click', () => {
         if(title.classList.contains('selected')) return;
         title.classList.add('selected');
         selectedTitle.classList.remove('selected');
         selectedTitle = title;
-        leagueTitles = document.querySelectorAll('.titles span');
+        titlesSpan = document.querySelectorAll('.titles span');
     })
 })
 
